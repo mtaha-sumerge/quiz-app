fdd8840d600d4779561b079725a8cdeecf64f079
mtaha-sumerge <mtaha@sumerge.com>
2025-10-07 19:20:33 +0300

    Creating quiz

--- src/main/java/QuizApplication.java
+++ src/main/java/QuizApplication.java
@@ -6,6 +6,7 @@ import java.util.*;
 
 public class QuizApplication {
 
+    // Change the number of questions as you prefer
     static int quizSize = 5;
 
     static List<Question> questions = List.of(
@@ -19,8 +20,8 @@ public class QuizApplication {
             new Question("Which planet is known as the Red Planet?", List.of("1. Earth", "2. Venus", "3. Mars", "4. Jupiter"), 2),
             new Question("What year was Java first released in?", List.of("1. 1991", "2. 1995", "3. 1999", "4. 2001"), 1),
             new Question("Which country hosted the 2022 FIFA World Cup?", List.of("1. Brazil", "2. Russia", "3. Germany", "4. Qatar"), 3),
-            new Question("What is the chemical symbol for gold?", List.of("1. Go", "2. Hg", "3. Au", "4. Ag"), 3)
-            );
+            new Question("What is the chemical symbol for gold?", List.of("1. Go", "2. Hg", "3. Au", "4. Ag"), 2)
+    );
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -29,23 +30,42 @@ public class QuizApplication {
         System.out.println("Enter your username: ");
         User user = new User(scanner.next());
 
-        // This set to handle the occurrence of a repeated random index
+        int mode;
+        System.out.println("Enter 1 or 2");
+        System.out.println("1. Create a Quiz (Choose your own questions)");
+        System.out.println("2. Take a Quiz");
+        mode = Integer.parseInt(scanner.next());
+
         Set<Question> quizQuestions = new HashSet<>();
-        Random random = new Random();
 
-        while (quizQuestions.size() < quizSize) {
-            int randomIndex = random.nextInt(questions.size());
-            quizQuestions.add(questions.get(randomIndex));
+        if (mode == 1) {
+            System.out.println("\nPick questions by entering their orders one by one");
+            for (int i = 0; i < questions.size(); i++) {
+                System.out.println(i+1 + ". " + questions.get(i).getText());
+            }
+            for (int i = 0; i < quizSize; i++) {
+                quizQuestions.add(questions.get(Integer.parseInt(scanner.next()) - 1));
+            }
+
+        } else if (mode == 2) {
+            // This set to handle the occurrence of a repeated random index
+            Random random = new Random();
+
+            while (quizQuestions.size() < quizSize) {
+                int randomIndex = random.nextInt(questions.size());
+                quizQuestions.add(questions.get(randomIndex));
+            }
         }
-
-        Quiz quiz = new Quiz(new ArrayList<Question>(quizQuestions));
+        Quiz quiz = new Quiz(new ArrayList<>(quizQuestions));
 
         List<Integer> answers = new ArrayList<>();
-        System.out.println("Quiz started!");
+        System.out.println("\nQuiz started!");
+        int i = 1;
         for (Question question : quizQuestions) {
             question.displayQuestion();
             System.out.println("Enter the choice number");
             answers.add(Integer.parseInt(scanner.next()) - 1);
+            System.out.println("You answered " + i++ + " question(s) of " + quizSize + '\n');
         }
         user.recordAnswers(answers);
         int score = quiz.calculateScore(answers);
